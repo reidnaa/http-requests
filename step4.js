794091bde75efb77bde4867cc5cde43e77705466
reidnaa <reidnaaykens@yahoo.ca>
2017-11-06 21:06:47 +0000

    step 4

---
+++ step4.js
@@ -0,0 +1,40 @@
+const https = require('https');
+
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step4.html'
+  };
+
+
+function getHTML(options , callback){
+
+
+
+https.get(options, function (res) {
+
+
+
+  res.setEncoding('utf8');
+
+
+  var data = "";
+  res.on('data', function (chunk) {
+
+    data += chunk;
+    console.log(data);
+  });
+
+
+
+
+});
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+getHTML(requestOptions , printHTML);
+
+
